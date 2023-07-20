86ed9b6981f95866c4a0eb72f83c39ab2da818bb
TaeHye0n <thk001229@gmail.com>
2023-07-20 21:41:05 +0900

    Refactor: Make Book CreateMethod  Book Entity 클래스에 Book 클래스를 생성하는 스태틱 메서드를 추가

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,4 +14,17 @@ public class Book extends Item {
 
     private String author;
     private String isbn;
+
+    //==생성 메서드==//
+    public static Book createBook(String name, int price, int stockQuantity, String author, String isbn) {
+        Book book = new Book();
+
+        book.setName(name);
+        book.setPrice(price);
+        book.setStockQuantity(stockQuantity);
+        book.setAuthor(author);
+        book.setIsbn(isbn);
+
+        return book;
+    }
 }
