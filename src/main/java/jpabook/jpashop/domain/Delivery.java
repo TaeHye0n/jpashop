10a25c420c15dfe1cbf1e740c3dd52e925b0d3c2
TaeHye0n <thk001229@gmail.com>
2023-07-22 17:46:21 +0900

    Feat: Read Simple Order Info API Version-01  - 간단 주문 조회 기능의 응답을 List<Order>로 하는 version-01  - 양방향 관계로 인해 JSON 직렬화 처리에서 무한루프에 빠지는 것을 막기 위해 @JsonIgnore 사용  - fetch타입이 LAZY인 경우 Jackson라이브러리로 프록시를 직렬화하게 되서 문제가 발생  - 위 문제를 해결하기 위해 hibernate5Module을 스프링 빈으로 등록하고 LAZY를 강제 초기화하고 프록시들은 NULL로 JSON에 넘김

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,12 +16,13 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
+    @JsonIgnore
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 
     @Embedded
     private Address address;
 
-    @Enumerated(EnumType.STRING) //ORDINAL은 1,2,3 같은 숫자로 들어감
+    @Enumerated(EnumType.STRING) //ORDINAL은 1,2,3 같은 숫자로 들어감 그래서 중간에 추가할 경우 문제가 발생함
     private DeliveryStatus status; //ENUM [READY(준비), COMP(배송)]
 }
