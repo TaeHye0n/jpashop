49fb1017c390e81d6a3d422c8132794e09a6122a
TaeHye0n <thk001229@gmail.com>
2023-07-25 14:01:36 +0900

    Feat: Order APi Ver-04  - 루트부터 xxOne 관계를 DTO직접 조회 방식으로 담고, 컬렉션 타입은 DTO로 따로 꺼내 루트로부터 담았던 DTO에 Set해줌  - 이 과정에서 N + 1문제가 발생함

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +26,7 @@ import java.util.stream.Collectors;
 public class OrderApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     /**
      * V1. 엔티티 직접 노출
@@ -76,6 +79,12 @@ public class OrderApiController {
                 .map(o -> new OrderDto(o))
                 .collect(Collectors.toList());
         return new Result(collect);
+    }
+
+    @GetMapping("/api/v4/orders")
+    public Result ordersV4() {
+        List<OrderQueryDto> list = orderQueryRepository.findOrderQueryDtos();
+        return new Result(list);
     }
 
     @Getter
